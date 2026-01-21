ddeef797eb14526f979043fa7cf78dc92c3f0f2a
dinofied <dino2002serbia@gmail.com>
2026-01-21 16:17:34 +0100

    Cleaning up parser logic, potential errors could be passed to cmd factory then to another error class later down the line

--- Command line interpreter/Parser.cpp
+++ Command line interpreter/Parser.cpp
@@ -7,22 +7,28 @@ ParsedCommand Parser::parsedCommand(const std::vector<std::string>& tokens) {
 	for (int i = 0; i < tokens.size(); i++) {
 		if (tokens[i] == "<") {
 			inRedirects = true;
-			if (result.redirection.hasInput == true) {
-				return {};
+			if (i == tokens.size() - 1 || tokens[i + 1] == "<" || tokens[i + 1] == ">" || tokens[i + 1] == ">>") {
+				return { {"error 1"} };
 			}
-			else result.redirection.hasInput == true;
+			if (result.redirection.hasInput == true) {
+				return { {"error 2"} };
+			}
+			else result.redirection.hasInput = true;
 			if (Command::isArgFile(tokens[i + 1])) {
 				result.redirection.inputFile = tokens[i + 1];
-				i++;
+				i ++;
 			}	
 			else {
-				return {};
+				return { {"error 3"}};
 			}
 		}
 		else if (tokens[i] == ">") {
 			inRedirects = true;
+			if (i == tokens.size() - 1 || tokens[i + 1] == "<" || tokens[i + 1] == ">" || tokens[i + 1] == ">>") {
+				return { {"error 4"} };
+			}
 			if (result.redirection.hasOutput == true || result.redirection.hasAppend == true) {
-				return {};
+				return { {"error 5"} };
 			}
 			else result.redirection.hasOutput = true;
 			if (Command::isArgFile(tokens[i + 1])) {
@@ -30,13 +36,16 @@ ParsedCommand Parser::parsedCommand(const std::vector<std::string>& tokens) {
 				i++;
 			}
 			else {
-				return {};
+				return { {"error 6"} };
 			}
 		}
 		else if (tokens[i] == ">>") {
 			inRedirects = true;
+			if (i == tokens.size() - 1 || tokens[i + 1] == "<" || tokens[i + 1] == ">" || tokens[i + 1] == ">>") {
+				return { {"error 7"} };
+			}
 			if (result.redirection.hasOutput == true || result.redirection.hasAppend == true) {
-				return {};
+				return { {"error 8"} };
 			}
 			else result.redirection.hasAppend = true;
 			if (Command::isArgFile(tokens[i + 1])) {
@@ -44,7 +53,7 @@ ParsedCommand Parser::parsedCommand(const std::vector<std::string>& tokens) {
 				i++;
 			}
 			else {
-				return {};
+				return { {"error 9"} };
 			}
 		}
 
